9dea2d15762b847882cf41d4f9cd37b7804233f7
hilak <hila.karimov@kaltura.com>
2022-06-27 15:54:41 -0400

    ILMS-118 Prevent multiple links to "My Media" from being added to the main Nav bar

--- lib.php
+++ lib.php
@@ -39,6 +39,12 @@ function local_mymedia_extend_navigation($navigation) {
         return;
     }
 
+    if (strpos($CFG->custommenuitems,'My Media') !== false)
+	{
+		//My Media is already part of the config, no need to add it again.
+		return;
+	}
+
 	$myMediaStr = "\nMy Media|/local/mymedia/mymedia.php";
 	$CFG->custommenuitems .= $myMediaStr;
 }
