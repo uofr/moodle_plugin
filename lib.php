efa39d529e160897e043a40921113b2c374ed63b
roileviK <roi.levi@kaltura.com>
2023-06-05 12:06:20 +0300

    SUP-36897 add my media link location config and allow setting it to side navigation to support classic theme

--- lib.php
+++ lib.php
@@ -21,6 +21,9 @@
  * @copyright  (C) 2014 Remote Learner.net Inc http://www.remote-learner.net
  */
 
+define('LOCAL_KALTURAMYMEDIA_LINK_LOCATION_TOP_NAVIGATION_MENU', 0);
+define('LOCAL_KALTURAMYMEDIA_LINK_LOCATION_SIDE_NAVIGATION_MENU', 1);
+
 /**
  * This function adds my media links to the navigation block
  * @param global_navigation $navigation a global_navigation object
@@ -33,19 +36,36 @@ function local_mymedia_extend_navigation($navigation) {
         return;
     }
 
+    // When on the admin-index page, first check if the capability exists.
+    // This is to cover the edge case on the Plugins check page, where a check for the capability is performed before the capability has been added to the Moodle mdl_capabilities
+    // table.
+    if ('admin-index' === $PAGE->pagetype) {
+        $exists = $DB->record_exists('capabilities', array('name' => 'local/mymedia:view'));
+
+        if (!$exists) {
+            return;
+        }
+    }
+
     $context = context_user::instance($USER->id);
 
     if (!has_capability('local/mymedia:view', $context, $USER)) {
         return;
     }
 
+    if (get_config('local_mymedia', 'link_location') == LOCAL_KALTURAMYMEDIA_LINK_LOCATION_SIDE_NAVIGATION_MENU) {
+        $nodehome = $navigation->get('home');
+        if (empty($nodehome)){
+            $nodehome = $navigation;
+        }
+        $mymedia = get_string('nav_mymedia', 'local_mymedia');
+        $icon = new pix_icon('my-media', '', 'local_mymedia');
+        $nodemymedia = $nodehome->add($mymedia, new moodle_url('/local/mymedia/mymedia.php'), navigation_node::NODETYPE_LEAF, $mymedia, 'mymedia', $icon);
+        $nodemymedia->showinflatnavigation = true;
+        return;
+    }
+
     $menuHeaderStr = get_string('nav_mymedia', 'local_mymedia');
-
-    if (strpos($CFG->custommenuitems,$menuHeaderStr) !== false) {
-		//My Media is already part of the config, no need to add it again.
-		return;
-	}
-
-	$myMediaStr = "\n$menuHeaderStr|/local/mymedia/mymedia.php";
-	$CFG->custommenuitems .= $myMediaStr;
+    $myMediaStr = "\n$menuHeaderStr|/local/mymedia/mymedia.php";
+    $CFG->custommenuitems .= $myMediaStr;
 }
