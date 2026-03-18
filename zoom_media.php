9bfa3c2157bd14ee7683e16da80000c036485625
Joel Dapiawen <joel.dapiawen@uregina.ca>
2026-03-18 16:13:03 -0600

    fix text column error from getrecord db

--- zoom_media.php
+++ zoom_media.php
@@ -7,7 +7,7 @@ require_login();
 $context = context_system::instance(); // Or your specific context
 require_capability('local/mymedia:view', $context);
 
-// Page Setup (Matching your Kaltura style)
+
 $header = get_string('zoom_media_title', 'local_mymedia');
 $PAGE->set_url('/local/mymedia/zoom_media.php');
 $PAGE->set_context($context);
@@ -25,23 +25,30 @@ if ($is_localhost) {
     echo '</div>';
 } else {
 
-    $zoom_tool = $DB->get_record('lti_types', ['baseurl' => 'https://applications.zoom.us/lti/advantage']);
-    
-    if ($zoom_tool) {
-        
-        $launchurl = new moodle_url('/mod/lti/launch.php', ['id' => $zoom_tool->id, 'container' => 'embed']);
-        $attr = [
-            'id' => 'contentframe',
-            'name' => 'contentframe',
-            'src' => $launchurl->out(false),
-            'width' => '100%',
-            'height' => '800px',
-            'allow' => 'autoplay *; fullscreen *; encrypted-media *; camera *; microphone *;'
-        ];
-        echo html_writer::tag('iframe', '', $attr);
-    } else {
-        echo $OUTPUT->notification("Zoom LTI Tool not found on this server.", 'error');
-    }
+   
+$zoomurl = 'https://applications.zoom.us/lti/advantage';
+
+
+$select = $DB->sql_compare_text('baseurl') . " = " . $DB->sql_compare_text(':url');
+
+
+$zoom_tool = $DB->get_record_select('lti_types', $select, ['url' => $zoomurl], '*', IGNORE_MULTIPLE);
+
+if ($zoom_tool) {
+
+    $launchurl = new moodle_url('/mod/lti/launch.php', ['id' => $zoom_tool->id, 'container' => 'embed']);
+    $attr = [
+        'id' => 'contentframe',
+        'name' => 'contentframe',
+        'src' => $launchurl->out(false),
+        'width' => '100%',
+        'height' => '800px',
+        'allow' => 'autoplay *; fullscreen *; encrypted-media *; camera *; microphone *;'
+    ];
+    echo html_writer::tag('iframe', '', $attr);
+} else {
+    echo $OUTPUT->notification("Zoom LTI Tool not found on this server.", 'error');
+}
 }
 
 echo $OUTPUT->footer();
