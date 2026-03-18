03b1971464ebe1f528d9f5f09c443c73780ae0e4
Trevor Cunningham <trevor.cunningham@uregina.ca>
2026-03-18 17:25:47 -0600

    adds support for LTI 1.1 testing

--- zvmlocallib.php
+++ zvmlocallib.php
@@ -109,6 +109,89 @@ function local_zvm_format_lti_instance_object($ltirequest) {
     return $lti;
 }
 
+/**
+ * This function converts an LTI request object into a properly formatted LTI request that can be consumed by Moodle's LTI local library.
+ * The function is modeled closely after @see lti_view().  The code was refactored because the original function relied too heavily on
+ * there being an LTI tool defined in the LTI activity instance table.
+ * @param array $ltirequest An array with parameters specifying some required information for an LTI launch.
+ * @param array $withblocks True if Moodle blocks are to be included on the page else false.
+ * @return string Returns HTML required to initiate an LTI launch.
+ */
+function local_zvm_request_lti_launch($ltirequest, $withblocks = true, $editor = null) {
+	$ltiVersion = 'LTI-1p0';
+
+    global $CFG, $USER;
+
+    if(is_null($editor))
+    {
+        $editor = 'tinymce';
+    }
+
+    $requestparams = array();
+
+    $lti = local_zvm_format_lti_instance_object($ltirequest);
+
+    $typeconfig = local_zvm_format_typeconfig($lti, $withblocks);
+
+    // This line was taken from @see lti_add_type.
+    // Create a salt value to be used for signing passed data to extension services
+    // The outcome service uses the service salt on the instance. This can be used
+    // for communication with services not related to a specific LTI instance.
+    $lti->servicesalt = uniqid('', true);
+
+    // If SSL is forced, use HTTPS.
+    $endpoint = $lti->toolurl;
+    if (lti_request_is_using_ssl()) {
+        $endpoint = $lti->securetool;
+    }
+
+    $requestparams = array_merge(lti_build_standard_request((object) $lti, null, false), lti_build_request((object) $lti, $typeconfig, $ltirequest['course']));
+    if(!isset($requestparams['resource_link_id'])) // fix to moodle 2.8 issue where this function (lti_build_request) does not set resource_link_id value
+    {
+        $requestparams['resource_link_id'] = $lti->id;
+    }
+
+    // Moodle by default uses the Moodle user id.  Overriding this parameter to user the Moodle username.
+    $requestparams['user_id'] = $USER->username;
+
+    // This block of code is loosly based off code from @see lti_view().
+    $urlparts = parse_url($CFG->wwwroot);
+    $requestparams['tool_consumer_instance_guid'] = $urlparts['host'];
+
+    $returnurlparams['unsigned'] = '0';
+    $returnurlparams['editor'] = $editor;
+
+    // Add the return URL. We send the launch container along to help us avoid frames-within-frames when the user returns.
+    $url = new moodle_url('/local/kaltura/service.php', $returnurlparams);
+    $requestparams['launch_presentation_return_url'] = $url->out(false);
+
+    $serviceurl = new moodle_url('/local/kaltura/service.php');
+    $requestparams['lis_outcome_service_url'] = $serviceurl->out(false);
+
+    // Add custom parameters
+    //$requestparams['custom_publishdata'] = local_kaltura_get_kaf_publishing_data();
+    //$requestparams['custom_publishdata_encoded'] = '1';
+    //$requestparams['custom_moodle_plugin_version'] = local_kaltura_get_config()->version;
+
+    if (isset($ltirequest['submission'])) {
+        $requestparams['assignment'] = $ltirequest['submission'];
+    }
+
+    $params = lti_sign_parameters($requestparams, $endpoint, 'POST', $lti->resourcekey, $lti->password);
+
+    local_zvm_strip_querystring($endpoint, $params);
+
+    $debuglaunch = 0;
+
+    $content = lti_post_launch_html($params, $endpoint, $debuglaunch);
+
+	error_log('zvm lti logging: '.print_r($ltirequest['module'],1));
+	error_log('zvm $endpoint: '.print_r($endpoint,1));
+	error_log('zvm $params: '.print_r($params,1));
+	
+    return $content;
+}
+
 function local_zvm_request_lti1p3_launch($ltirequest, $withblocks = true, $editor = null) {
 	global $SESSION;
 
@@ -195,4 +278,41 @@ function local_zvm_get_config() {
     //$configsettings->redirection_uris = 'https://applications.zoom.us/lti/advantage/oauth/complete';
     
     return $configsettings;
+}
+
+/**
+ * This function formats an array that is passed to mod_lti locallib.php functions.
+ * @param object $lti An object returned from @see local_kaltura_format_lti_instance_object().
+ * @param bool $withblocks Set to true to display blocks.  Otherwise false.
+ * @return array An array formatted for use by mod_lti locallib.php functions.
+ */
+function local_zvm_format_typeconfig($lti, $withblocks = true) {
+    $typeconfig = array();
+    $typeconfig['sendname'] = $lti->instructorchoicesendname;
+    $typeconfig['sendemailaddr'] = $lti->instructorchoicesendemailaddr;
+    $typeconfig['customparameters'] = $lti->instructorcustomparameters;
+    $typeconfig['acceptgrades'] = $lti->instructorchoiceacceptgrades;
+    $typeconfig['allowroster'] = $lti->instructorchoiceallowroster;
+    $typeconfig['launchcontainer'] = local_zvm_get_lti_launch_container($withblocks);
+
+    return $typeconfig;
+}
+
+/**
+ * This function is based off of the code from @see lti_view().
+ * @param string $endpoint The URL to access the KAF LTI tool.
+ * @param string $params The signed parameters returned by @see lti_sign_parameters().
+ */
+function local_zvm_strip_querystring($endpoint, $params) {
+    $endpointurl = new moodle_url($endpoint);
+    $endpointparams = $endpointurl->params();
+
+    // Strip querystring params in endpoint url from $parms to avoid duplication.
+    if (!empty($endpointparams) && !empty($parms)) {
+        foreach (array_keys($endpointparams) as $paramname) {
+            if (isset($parms[$paramname])) {
+                unset($parms[$paramname]);
+            }
+        }
+    }
 }
