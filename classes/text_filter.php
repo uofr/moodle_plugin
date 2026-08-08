e60e18c50be822f47fc15a5a7f585fb613287e9f
Trevor Cunningham <trevor.cunningham@uregina.ca>
2026-08-07 19:43:48 -0600

    remove extraneous commented code

--- classes/text_filter.php
+++ classes/text_filter.php
@@ -220,8 +220,6 @@ class text_filter extends \filter_kaltura_base_text_filter {
 		
 		//error_log('filter/kaltura/ - found entryid: '.$entry_id);
 		
-		//limit the zoom replacement to specific courses for now
-		//$zoom_courses = array('37619','31544','39149','31924');
 		$course_id = $COURSE->id;
 			
 		// Check on Zoom if course has Zoom channel
@@ -272,11 +270,6 @@ class text_filter extends \filter_kaltura_base_text_filter {
 			
 			if ($zoomchannel) {
 				$added_video = $zoomapi->add_video_to_channel($zoomchannel->value, $zoomclip->clip_id);
-			
-				//$adhocsynctask = \mod_zoomvideo\task\adhoc_update_channel_permissions::instance($course_id);
-				//\core\task\manager::queue_adhoc_task($adhocsynctask);
-				
-				//sleep(0.5); // give it a moment to let the permissions update	
 			}
 			
 			
